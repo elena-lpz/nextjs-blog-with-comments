1ed9774f5963862e8611f513e965eda5076604da
Elena Lopez <emloppal@gmail.com>
2025-06-20 15:52:46 +0100

    fix: add homepage and style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer>
-      <p className="flex justify-center font-medium">All rights reserved</p>
+      <p className="absolute bottom-0 flex justify-center font-medium">
+        All rights reserved
+      </p>
     </footer>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,45 @@
+import femalegazetext from "@/../public/logos/femalegazetext.svg";
+import tonjebboe1 from "@/../public/photography/tonjebboe1.jpg";
+import vector from "@/../public/misc/vector.png";
+import Image from "next/image";
+
 export default function Main() {
   return (
-    <>
-      <h1 className="flex justify-center">This is my main section</h1>
-    </>
+    <section className=" m-4 md:m-15">
+      <Image
+        src={vector}
+        alt="vector image"
+        width={380}
+        height={75}
+        className="md:-ms-15 md:pb-6"
+      />
+      <div className=" flex flex-col-reverse md:flex-row items-start justify-between">
+        <p className="flex md:w-[30%] text-md md:text-lg pt-10 md:pt-0">
+          Through the Female Gaze, light finds its voice. A quiet revolution
+          framed in moments, where women capture the world as only they can see
+          it.
+        </p>
+        <div className="flex flex-col items-end">
+          <Image
+            src={tonjebboe1}
+            alt="A woman looking at deer in the museum of national history"
+            width={400}
+            height={200}
+            className="w-[70%] md:w-[50%] xl:w-[70%] pt-10 md:pt-0"
+          />
+          <p className="text-neutral-400 text-xs w-[70%] text-right md:w-[100%] md:text-sm pt-1 md:pt-2">
+            © Tonje Boe Birkeland, Character #III
+          </p>
+        </div>
+      </div>
+
+      <Image
+        src={femalegazetext}
+        alt="Logo in red of The Female Gaze"
+        width={2400}
+        height={400}
+        className="absolute bottom-10 pt-10 md:pt-0 w-[90dvw] md:w-[90dvw] "
+      />
+    </section>
   );
 }
